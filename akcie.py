a95cdef46561d0b1b26302d61f8a1116ff346b3c
Tim van Rossum <trvanrossum@gmail.com>
2019-02-16 21:49:30 +0100

    Made akcie.py a lot easier to understand.

--- akcie.py
+++ akcie.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- 
-import pymysql.cursors
-import time
+# -*- coding: utf-8 -*-
 import datetime
 from barkas import Barkas
 from tkinter import Tk, Frame, BOTH, Label, StringVar, CENTER
@@ -9,6 +7,8 @@ from tkinter import Tk, Frame, BOTH, Label, StringVar, CENTER
 DATUM = "2015-11-06"
 DRANK = "Bier zwembadfeest"
 GROEPERINGEN = ['Nobel', 'Krat', 'Bestuur 122', 'Chaos', 'Spetter', 'Quast', 'Octopus', 'McClan', 'Kurk', 'Apollo', 'Schranz', 'Asene', 'Kielzog', 'Scorpios', 'Fabula', 'TDC 66']
+CONSUMPTIES = {'Fris': 2, 'Pul Fris': 6, 'Safari': 3, 'Apfelkorn': 3, 'Eristoff': 3, 'Jagermeister': 3, 'Likeur 43': 3, 'Pitcher bier': 6, 'Peach Tree ': 3}
+S50 = {'bacardi razz': 40, 'honingwijn': 15}
 
 class Example(Frame):
 
@@ -22,14 +22,13 @@ class Example(Frame):
         self.scores = {}
         for i, g in enumerate(GROEPERINGEN):
             self.names[i] = StringVar()
-            self.scores[i] = StringVar()   
+            self.scores[i] = StringVar()
 
         self.initUI()
-        self.update_scores()
+        #self.update_scores()
     
     def initUI(self):
         self.parent.title("Scores")
-
         for i, g in enumerate(GROEPERINGEN):
             w = Label(self, textvariable=self.names[i], font=("Helvetica", 15))
             w.place(rely= 0.95 * i / float(len(GROEPERINGEN)) + 0.05, relx=0.3, anchor=CENTER)
@@ -51,29 +50,12 @@ class Example(Frame):
         scores = {}
         for g in GROEPERINGEN:
             score = 2 * self.barkas.get_number_of_beers(date, g)
-            score += 2 * self.barkas.get_number_of_consumptions(date, g, 'Fris')
-            score += 6 * self.barkas.get_number_of_consumptions(date, g, 'Pul Fris')
-            score += 3 * self.barkas.get_number_of_consumptions(date, g, 'Safari')
-            score += 3 * self.barkas.get_number_of_consumptions(date, g, 'Apfelkorn')
-            score += 3 * self.barkas.get_number_of_consumptions(date, g, 'Eristoff')
-            score += 3 * self.barkas.get_number_of_consumptions(date, g, 'Jagermeister')
-            score += 3 * self.barkas.get_number_of_consumptions(date, g, 'Likeur 43')
-            score += 6 * self.barkas.get_number_of_consumptions(date, g, 'Pitcher bier')
-            score += 40 * self.barkas.get_number_of_s50(date, g, 'bacardi razz')
-            score += 15 * self.barkas.get_number_of_s50(date, g, 'honingwijn')
-            score += 3 * self.barkas.get_number_of_consumptions(date, g, 'Peach Tree ')
-            score += 2 * self.barkas.get_number_of_beers(datetime.date(2015,11,30), g)
-            score += 2 * self.barkas.get_number_of_consumptions(datetime.date(2015,11,30), g, 'Fris')
-            score += 6 * self.barkas.get_number_of_consumptions(datetime.date(2015,11,30), g, 'Pul Fris')
-            score += 3 * self.barkas.get_number_of_consumptions(datetime.date(2015,11,30), g, 'Safari')
-            score += 3 * self.barkas.get_number_of_consumptions(datetime.date(2015,11,30), g, 'Apfelkorn')
-            score += 3 * self.barkas.get_number_of_consumptions(datetime.date(2015,11,30), g, 'Eristoff')
-            score += 3 * self.barkas.get_number_of_consumptions(datetime.date(2015,11,30), g, 'Jagermeister')
-            score += 3 * self.barkas.get_number_of_consumptions(datetime.date(2015,11,30), g, 'Likeur 43')
-            score += 6 * self.barkas.get_number_of_consumptions(datetime.date(2015,11,30), g, 'Pitcher bier')
-            score += 40 * self.barkas.get_number_of_s50(datetime.date(2015,11,30), g, 'bacardi razz')
-            score += 15 * self.barkas.get_number_of_s50(datetime.date(2015,11,30), g, 'honingwijn')
-            score += 3 * self.barkas.get_number_of_consumptions(datetime.date(2015,11,30), g, 'Peach Tree ')
+
+            for (name, mult) in CONSUMPTIES.items():
+                score += mult * self.barkas.get_number_of_consumptions(date, g, name)
+
+            for (name, mult) in S50.items():
+                score += mult * self.barkas.get_number_of_s50(date, g, name)
 
             scores[g] = score
 
@@ -91,6 +73,7 @@ def main():
   
     root = Tk()
     root.geometry("800x500")
+    print('Running')
     app = Example(root)
     root.mainloop()  
 
---
+++ bestellingen.py
@@ -0,0 +1,9 @@
+import datetime
+from barkas import Barkas
+
+class Bestellingen():
+
+    groepering = ''
+
+    def __init__(self, _groep):
+        groepering = _groep
