beac35b0898bdfd9c2640c88fe1fc9838e3d5044
Tim van Rossum <trvanrossum@gmail.com>
2019-02-17 14:57:24 +0100

    Orders are now randomized.

--- chaos.py
+++ chaos.py
@@ -3,6 +3,7 @@
 import datetime
 from barkas import Barkas
 from tkinter import Tk, Frame, BOTH, Label, StringVar, CENTER
+import numpy as np
 
 DATUM = "2015-11-06"
 DRANK = "Bier zwembadfeest"
@@ -13,6 +14,10 @@ SCORES = {}
 BESTELLINGEN = {}
 
 class Example(Frame):
+
+    MAP_CONS = {}
+    MAP_S50 = {}
+    LATEST_CHECK_MINUTES = 0
 
     def __init__(self, parent):
         Frame.__init__(self, parent, background="white")   
@@ -31,6 +36,8 @@ class Example(Frame):
             SCORES[g] = 0.0
 
         self.initUI()
+        self.MAP_CONS = self.create_random_mapping_consumptions()
+        self.MAP_S50 = self.create_random_mapping_s50()
         self.update_scores_test()
         #self.update_scores()
 
@@ -88,9 +95,62 @@ class Example(Frame):
         # Return 15.0 minus the distance. This is very hacky, but it awards more points for orders very close to us.
         return 15.0 - dist
 
+    # These two functions are for the creation of random mappings. Consumptions are mapped to consumptions
+    # and S50 is mapped to S50. Beer is not mapped to anything else.
+    def create_random_mappings(self):
+        map_cons = self.create_random_mapping(CONSUMPTIES)
+        map_s50 = self.create_random_mapping(S50)
+        return (map_cons, map_s50)
+
+    def create_random_mapping(self, input):
+        map_from = input
+        map_to = np.random.permutation(map_from)
+        res = {}
+        for i in range(len(map_from)):
+            res[map_from[i]] = map_to[i]
+        return res
+
+    def randomize_orders(self, orders):
+        res = {}
+
+        # Do not remap beer.
+        res['Bier'] = orders['Bier']
+
+        # If an order contains X of item A and item A is mapped to item B, then the randomized order contains X amount of item B.
+        for k, v in self.MAP_CONS:
+            res[v] = orders[k]
+
+        # Same, but for S50.
+        for k, v in self.MAP_S50:
+            res[v] = orders[k]
+
+        return res
+
+
+
+    # The random maps need to be re-randomized every 30 minutes. This function checks if 30 minutes have passed already.
+    def check_if_maps_need_updating(self):
+        current_time = datetime.datetime.now()
+        current_time_mins = current_time.minute
+
+        # If 30 minutes have passed, the min_mod_30 variable should be less than the previous one.
+        checked = False
+        min_mod_30 = current_time_mins % 30
+        if min_mod_30 < self.LATEST_CHECK_MINUTES:
+            checked = True
+
+        self.LATEST_CHECK_MINUTES = min_mod_30
+        return checked
+
 
     def update_scores(self):
         print("Update")
+
+        # Check if randomized maps need updating.
+        if self.check_if_maps_need_updating():
+            (map_c, map_s) = self.create_random_mappings()
+            self.MAP_CONS = map_c
+            self.MAP_S50 = map_s
 
         # Get orders of Chaos
         orders_Chaos = self.get_total_orders_of_group('Chaos')
@@ -103,11 +163,13 @@ class Example(Frame):
             (changed, new_orders) = self.compare_old_and_new_orders(g_orders_old, g_orders_new)
             # If a new order has been placed by a group, update the score of that group.
             if changed:
-                similarity = self.chi_square_sim(self.normalize_orders(orders_Chaos), self.normalize_orders(new_orders))
+                # Randomize the new order of that group, and then compare it with what Chaos has ordered.
+                random_orders = self.randomize_orders(new_orders)
+                similarity = self.chi_square_sim(self.normalize_orders(orders_Chaos), self.normalize_orders(random_orders))
                 SCORES[g] += similarity
 
-
-        self.after(3000, self.update_scores)
+        # Keep running this function every 10 seconds.
+        self.after(10000, self.update_scores)
 
     def update_scores_test(self):
         print("Update")
