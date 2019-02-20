be6e9fb9946f1c7275f5fb3897f4a5626ca4b790
Alexander van Gessel <AI0867@gmail.com>
2019-02-20 16:47:34 +0100

    Avoid CORS blocking

--- server.py
+++ server.py
@@ -12,13 +12,19 @@ CERTFILE = 'fullchain.pem'
 KEYFILE = 'privkey.pem'
 
 class ChaosRequestHandler(http.server.BaseHTTPRequestHandler):
+    def do_HEAD(self):
+        self.send_response(204)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
     def do_GET(self):
         if self.server.scoredump is None:
             self.send_response(204)
+            self.send_header('Access-Control-Allow-Origin', '*')
             self.end_headers()
         else:
             self.send_response(200)
             self.send_header('Content-Type', 'application/json')
+            self.send_header('Access-Control-Allow-Origin', '*')
             self.end_headers()
             self.wfile.write(self.server.scoredump)
     def do_POST(self):
